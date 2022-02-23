a197e88f0415d7c0b1d7fcde4d885a8c5b355e80
janealom084 <janealom084@gmail.com>
2022-02-23 20:48:26 +0600

    some data showed in website

--- countries.js
+++ countries.js
@@ -5,6 +5,19 @@ const loadCountires = () => {
 }
 loadCountires();
 
+const countriesDiv = document.getElementById('countries')
 const displyCountries = countries => {
-    console.log(countries);
+    countries.forEach(country => {
+        // console.log(country)
+        const div = document.createElement('div');
+        div.classList.add('country');
+        const h2 = document.createElement('h2');
+        h2.innerText = country.name.common;
+        div.appendChild(h2);
+        const p = document.createElement('p');
+        p.innerText = `Capital: ${country.capital} Area: ${country.area} Population: ${country.population}`
+        div.appendChild(p);
+        countriesDiv.appendChild(div);
+
+    })
 }
